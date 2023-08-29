19aec6880408c3594fe465db33fc6862fa289ab5
SandroKE <koomealessandro@gmail.com>
2023-08-29 17:53:01 +0300

    initial commit

--- index.php
+++ index.php
@@ -1,4 +1,45 @@
+<?php
+require "vendor/autoload.php";
 
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
+use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
+
+$result = Builder::create()
+    ->writer(new PngWriter())
+    ->writerOptions([])
+    ->data('https://github.com/AlessandroKE')
+    ->encoding(new Encoding('UTF-8'))
+    ->errorCorrectionLevel(new ErrorCorrectionLevelHigh())
+    ->size(300)
+    ->margin(10)
+    ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
+   /*  ->logoPath(__DIR__.'/assets/symfony.png')
+    ->logoResizeToWidth(50)
+    ->logoPunchoutBackground(true) */
+    ->labelText('Github Profile')
+    ->labelFont(new NotoSans(20))
+    ->labelAlignment(new LabelAlignmentCenter())
+    ->validateResult(false)
+    ->build();
+
+
+
+    // Directly output the QR code
+    header('Content-Type: '.$result->getMimeType());
+    echo $result->getString();
+
+    // Save it to a file
+    $result->saveToFile(__DIR__.'/qrcode.png');
+
+    // Generate a data URI to include image data inline (i.e. inside an <img> tag)
+    $dataUri = $result->getDataUri();
+?>
+<!-- 
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -46,3 +87,4 @@
      
     </body>
 </html>
+ -->
